fa0433b38436f9d5e72b0cc384902c46c067a50f
jinmu333 <jinmu333@qq.com>
2020-01-21 02:21:43 +0800

    use bootstrap

--- flask_demo/auth.py
+++ flask_demo/auth.py
@@ -41,7 +41,7 @@ def register():
     return render_template('auth/register.html')
 
 
-@bp.route('/login', methods=('GET', 'POST'))
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
